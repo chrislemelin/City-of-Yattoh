9305323301d9d91e9e7f8ae2a0fbf96e49929346
Chris Lemelin <clemelin2@gmail.com>
2018-01-25 17:02:25 -0500

    canvas scaling things

---
+++ Assets/Project/UI/UIScaling.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScaling : MonoBehaviour {
+
+    private Vector3 scale = new Vector3(-.75f,.75f,1);
+
+    public void Start()
+    {
+        transform.localScale = scale;
+    }
+}
