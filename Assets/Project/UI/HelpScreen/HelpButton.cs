0e13d9207a71b17dee657c19a7244d53a42cead2
Chris Lemelin <clemelin2@gmail.com>
2018-02-20 21:22:00 -0500

    added help button explaining the ui

---
+++ Assets/Project/UI/HelpScreen/HelpButton.cs
@@ -0,0 +1,20 @@
+﻿using Placeholdernamespace.Common.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButton : MonoBehaviour {
+
+    [SerializeField]
+    GameObject HelpScreen;
+
+    public void Start()
+    {
+        GetComponent<OnPointerDownListener>().pressed += HelpClick;
+    }
+
+    public void HelpClick()
+    {
+        HelpScreen.SetActive(!HelpScreen.activeSelf);
+    }
+}
